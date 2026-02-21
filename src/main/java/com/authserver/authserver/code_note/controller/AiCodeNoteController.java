c40dd59bdbc0ed11fe253a89dbf25f988ac03579
dhruv4023 <dhruv20345@gmail.com>
2026-02-21 15:40:29 +0530

    ai controller

---
+++ src/main/java/com/authserver/authserver/code_note/controller/AiCodeNoteController.java
@@ -0,0 +1,30 @@
+package com.authserver.authserver.code_note.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.authserver.authserver.base.response.BaseResponse;
+import com.authserver.authserver.code_note.CodeNoteService;
+import com.authserver.authserver.code_note.entry.CodeNoteEntry;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/ai-code-note")
+public class AiCodeNoteController {
+
+    private final CodeNoteService codeNoteService;
+
+    AiCodeNoteController(CodeNoteService codeNoteService) {
+        this.codeNoteService = codeNoteService;
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<BaseResponse<CodeNoteEntry>> updateCodeNote(@RequestBody CodeNoteEntry entry,
+            @PathVariable Long id) {
+        return codeNoteService.update(id, entry);
+    }
+}
--- src/main/java/com/authserver/authserver/code_note/controller/CodeNoteController.java
+++ src/main/java/com/authserver/authserver/code_note/controller/CodeNoteController.java
@@ -1,9 +1,10 @@
-package com.authserver.authserver.code_note;
+package com.authserver.authserver.code_note.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.authserver.authserver.base.controllers.BaseController;
+import com.authserver.authserver.code_note.CodeNoteService;
 import com.authserver.authserver.code_note.entry.CodeNoteEntry;
 
 @RestController
